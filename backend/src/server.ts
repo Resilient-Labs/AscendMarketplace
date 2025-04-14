78c46119347605c7325fcf09e584663e418ade36
Sebastian Vivas <seb.vivas.dev@gmail.com>
2025-04-14 19:54:29 -0400

    Issue_1

---
+++ backend/src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
