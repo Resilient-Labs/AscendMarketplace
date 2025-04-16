d9a4ddff95b6ba861845f426b00dda807f98ed76
Sebastian Vivas <seb.vivas.dev@gmail.com>
2025-04-15 20:36:35 -0400

    Added ESLint to backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
+// import express, { Request, Response } from "express";
+// import cors from "cors";
+import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -11,10 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_req, res) => {
   res.send("Express + TypeScript Server is running");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
